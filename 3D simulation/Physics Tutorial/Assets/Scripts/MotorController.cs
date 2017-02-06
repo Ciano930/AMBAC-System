c0074fe11f17f10ff261a04e22002e39f570efde
Cian O'Siorain <Ciano930@live.de>
2017-02-06 18:22:55 +0000

    Completed first Motor control
    The first attempt to control the motor has succeeded! However
    limitations of the robots frame have now come full circle.
    As such I will need to restart the design for the simulation robot.
    While I'm doing this I think I will look at the physical robot and try
    to resolve the hinge issues for universal joints.

---
+++ 3D simulation/Physics Tutorial/Assets/Scripts/MotorController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotorController : MonoBehaviour {
+
+    public GameObject jointPositioner;
+    public GameObject targetingPointer;
+
+    public HingeJoint jointHinge;
+    public JointLimits jointLimit;
+    public JointMotor jointMotor;
+
+    public float rotDifference;
+    private float targetRot;
+
+    public int option;
+    private bool run;
+
+    //The main logic for these will change depending on which of these motors we are using and so a value will be set ahead of time.
+    // Use this for initialization
+    void Start()
+    {
+        if (option == 1 || option == 2) // will Either be a single Joint like the knee or elbow or a universal joint like the shoulder or hip 
+        {
+            run = true;
+
+            //now lets set up the motor
+            jointMotor.force = 90;
+            jointMotor.targetVelocity = 0;
+            jointMotor.freeSpin = false;
+            jointHinge.motor = jointMotor;
+            jointHinge.useMotor = true;
+
+            jointHinge.limits = jointLimit;
+        }
+        else
+        {
+            Debug.Log(option + " is not a valid option");
+            run = false;
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(run)
+        {
+            switch(option)
+            {
+                case 1:
+                    SingleHingeUpdateMotor();
+                    break;
+                case 2:
+                    break;
+            }
+        }
+	
+	}
+
+    void SingleHingeUpdateMotor()
+    {
+        //first we need the actual angle from the joint from the Quaternion rotation.
+        float angle = targetingPointer.transform.eulerAngles.x;
+
+        Debug.Log("angle:" + angle);
+        if(angle > 180)
+        {
+            //angle = angle - 360;
+        }
+
+        jointLimit.min = -angle;
+        jointMotor.targetVelocity = -90;
+        jointHinge.limits = jointLimit;
+
+        jointHinge.motor = jointMotor;
+    }
+
+
+    void UniversalJointUpdateMotor()
+    {
+
+    }
+}
