9747371638dfdc7229e680fbcac805abea33f39c
Cian O'Siorain <Ciano930@live.de>
2017-02-28 20:47:17 +0000

    Completed leg control

---
+++ 3D simulation/Physics Tutorial/Assets/Scripts/KneeHipShperes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KneeHipShperes : MonoBehaviour {
+
+
+    public GameObject knee;
+    public GameObject hip;
+    public GameObject self;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateAroundKneeHip();
+	}
+
+    void updateAroundKneeHip()
+    {
+        self.transform.position = new Vector3 (knee.transform.position.x, hip.transform.position.y, knee.transform.position.z);
+    }
+} 
